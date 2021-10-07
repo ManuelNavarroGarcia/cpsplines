c8478f5dec4f45e86fabdb7863ca9dfa8ac610ec
Manuel Navarro <100423470@alumnos.uc3m.es>
2021-10-07 14:03:37 +0200

    refactor test_b_matrix

--- tests/test_b_matrix.py
+++ tests/test_b_matrix.py
@@ -2,8 +2,10 @@ import numpy as np
 import pytest
 
 from template.psplines.bspline_basis import BsplineBasis
+from template.utils.weighted_b import get_weighted_B, get_idx_fitting_region
 
-B2 = (1 / 8) * np.array(
+
+B1 = (1 / 8) * np.array(
     [
         [4, 4, 0, 0, 0, 0, 0],
         [1, 6, 1, 0, 0, 0, 0],
@@ -19,7 +21,7 @@ B2 = (1 / 8) * np.array(
     ]
 )
 
-B3 = (1 / 750) * np.array(
+B2 = (1 / 750) * np.array(
     [
         [125, 500, 125, 0, 0, 0, 0, 0, 0],
         [8, 311, 404, 27, 0, 0, 0, 0, 0],
@@ -35,7 +37,7 @@ B3 = (1 / 750) * np.array(
     ]
 )
 
-B4 = (1 / 240000) * np.array(
+B3 = (1 / 240000) * np.array(
     [
         [10000, 110000, 110000, 10000, 0, 0, 0, 0, 0, 0, 0],
         [81, 28156, 137846, 71516, 2401, 0, 0, 0, 0, 0, 0],
@@ -51,17 +53,117 @@ B4 = (1 / 240000) * np.array(
     ]
 )
 
+BV1 = (1 / 50) * np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 25, 25, 0, 0, 0, 0, 0],
+        [0, 4, 37, 9, 0, 0, 0, 0],
+        [0, 0, 16, 33, 1, 0, 0, 0],
+        [0, 0, 1, 33, 16, 0, 0, 0],
+        [0, 0, 0, 9, 37, 4, 0, 0],
+        [0, 0, 0, 0, 25, 25, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+)
+BV2 = (1 / 162) * np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 27, 108, 27, 0, 0, 0, 0],
+        [0, 0, 1, 60, 93, 8, 0, 0, 0],
+        [0, 0, 0, 8, 93, 60, 1, 0, 0],
+        [0, 0, 0, 0, 27, 108, 27, 0, 0],
+        [0, 0, 0, 0, 1, 60, 93, 8, 0],
+        [0, 0, 0, 0, 0, 8, 93, 60, 1],
+        [0, 0, 0, 0, 0, 0, 27, 108, 27],
+    ]
+)
+
+BV3 = (1 / 98) * np.array(
+    [
+        [49, 49, 0, 0, 0, 0, 0, 0, 0],
+        [4, 69, 25, 0, 0, 0, 0, 0, 0],
+        [0, 16, 73, 9, 0, 0, 0, 0, 0],
+        [0, 0, 36, 61, 1, 0, 0, 0, 0],
+        [0, 0, 1, 61, 36, 0, 0, 0, 0],
+        [0, 0, 0, 9, 73, 16, 0, 0, 0],
+        [0, 0, 0, 0, 25, 69, 4, 0, 0],
+        [0, 0, 0, 0, 0, 49, 49, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+)
+
+# Test if the matrix B is computed properly using different parameters. Since at
+# the end we are interested on the weighted matrices, the output is the list of
+# weighted matrices from `get_weighted_B`.
+
 
 @pytest.mark.parametrize(
-    "x_sam, deg, n_int, B",
+    "x_sam, deg, n_int, prediction, B",
     [
-        (np.linspace(0, 10, 11), 2, 5, B2),
-        (np.linspace(0, 10, 11), 3, 6, B3),
-        (np.linspace(0, 10, 11), 4, 7, B4),
+        ([np.linspace(0, 10, 11)], [2], [5], [{}], [B1]),
+        ([np.linspace(0, 10, 11)], [3], [6], [{}], [B2]),
+        ([np.linspace(0, 10, 11)], [4], [7], [{}], [B3]),
+        ([np.linspace(0, 5, 6)], [2], [3], [{"backwards": -1, "forward": 8}], [BV1]),
+        (
+            [np.linspace(0, 6, 7), np.linspace(-2.5, 3.7, 8)],
+            [3, 2],
+            [4, 5],
+            [{"backwards": -2.5}, {"forward": 5}],
+            [BV2, BV3],
+        ),
     ],
 )
-def test_B_matrix(x_sam, deg, n_int, B):
+def test_B_matrix(x_sam, deg, n_int, prediction, B):
+    bspline = []
+    for x, d, n, pred in zip(x_sam, deg, n_int, prediction):
+        bsp = BsplineBasis(deg=d, xsample=x, n_int=n, prediction=pred)
+        bsp.get_matrix_B()
+        bspline.append(bsp)
 
-    bsp = BsplineBasis(deg=deg, xsample=x_sam, n_int=n_int)
-    bsp.get_matrix_B()
-    np.testing.assert_allclose(bsp.matrixB, B)
+    B_out = get_weighted_B(bspline_bases=bspline)
+
+    for P, Q in zip(B_out, B):
+        np.testing.assert_allclose(P, Q)
+
+
+# Test correct ranges of the fitting region given a regressor sample
+@pytest.mark.parametrize(
+    "x_sam, deg, n_int, prediction, x_range",
+    [
+        (
+            [np.linspace(0, 5, 6)],
+            [2],
+            [3],
+            [{"backwards": -1, "forward": 8}],
+            (slice(1, 7, None),),
+        ),
+        (
+            [np.linspace(0, 6, 7), np.linspace(-2.5, 3.7, 8)],
+            [3, 2],
+            [4, 5],
+            [{"backwards": -2.5}, {"forward": 5}],
+            (slice(2, 9, None), slice(0, 8, None)),
+        ),
+        (
+            [np.linspace(0, 8, 71), np.linspace(-2, 4, 83), np.linspace(10, 11, 10)],
+            [5, 4, 6],
+            [7, 8, 3],
+            [{"forward": 8.5}, {}, {"backwards": 9, "forward": 12.34}],
+            (slice(0, 71, None), slice(0, 83, None), slice(3, 13, None)),
+        ),
+    ],
+)
+def test_get_idx_fit(x_sam, deg, n_int, prediction, x_range):
+    bspline = []
+    for x, d, n, pred in zip(x_sam, deg, n_int, prediction):
+        bsp = BsplineBasis(deg=d, xsample=x, n_int=n, prediction=pred)
+        bsp.get_matrix_B()
+        bspline.append(bsp)
+
+    range_out = get_idx_fitting_region(bspline_bases=bspline)
+
+    for slice_out, slice_in in zip(range_out, x_range):
+        assert slice_in == slice_out
