6a13eef07582bc93dc76ce7f23bdda6d75e0b1c1
Manuel Navarro <100423470@alumnos.uc3m.es>
2021-10-07 17:57:27 +0200

    docstring plot_curves

--- template/graphics/plot_one_smoothing.py
+++ template/graphics/plot_one_smoothing.py
@@ -21,20 +21,84 @@ def plot_curves(
     figsize: Tuple[Union[int, float]] = (15, 10),
 ) -> Tuple[matplotlib.figure.Figure, plt.axes]:
 
-    fig, ax = plt.subplots(figsize=figsize)
+    """
+    Plot a set of curves fitted using the method `OneSmoothing`.
 
+    Parameters
+    ----------
+    fittings : Iterable[OneSmoothing]
+        An iterable of fitted `OneSmoothing` objects
+    col_curve : Optional[Iterable[str]], optional
+        An iterable with the colours used to graph the curves (in the same order
+        as the corresponding curve). If None, the colours are chosen as random.
+        By default, None.
+    x : Optional[Iterable[np.ndarray]], optional
+        An iterable with the x-coordinates of points to be plotted. Each element
+        contain a set of coordinates. By default, None.
+    y : Optional[Iterable[np.ndarray]], optional
+        An iterable with the y-coordinates of points to be plotted. By default,
+        None.
+    col_pt : Optional[Iterable[str]], optional
+        An iterable with the colours used to plot the points (in the same order
+        as the corresponding set of points). If None, the colours are chosen as
+        random. By default, None.
+    alpha : Union[int, float], optional
+        The transparency level of the points. Only applies when points are
+        plotted. By default, 0.25.
+    prediction_step : Iterable[Union[int, float]], optional
+        The step used to produce equidistant extra points at the prediction
+        regions so the graph of the curves seems smoother (if this is not
+        applied, the only points at the prediction points are the knots). The
+        first element of the iterable is the step on the backwards prediction
+        and the second element corresponds to the step on the forward
+        prediction. By default, (0.5, 0.5).
+    knot_positions : bool, optional
+        If True, the positions where the inner knots are located are marked as
+        grey vertical lines. By default, False.
+    constant_constraints : bool, optional
+        If True, horizontal lines at the threshold of the zero-order derivative
+        constraints are plotted with red dashed lines. By default, False.
+    figsize : Tuple[Union[int, float]], optional
+        The size of the figure. By default, (15, 10).
+
+    Returns
+    -------
+    Tuple[matplotlib.figure.Figure, plt.axes]
+        A tuple containing the figure object and the axis.
+
+    Raises
+    ------
+    ValueError
+        If the number of the curve colours do not coincide with the number of
+        curves.
+    ValueError
+        If the number of the points colours do not coincide with the number of
+        set of points.
+    AttributeError
+        If any of the curve objects is not fitted yet.
+    """
+
+    fig, ax = plt.subplots(figsize=figsize)
+    # If `col_curve` is None, create the colours randomly
     if col_curve is None:
         col_curve = [
             "#" + "".join([random.choice("0123456789ABCDEF") for _ in range(6)])
             for _ in range(len(fittings))
         ]
 
+    if len(col_curve) != len(fittings):
+        raise ValueError("Number of colours and number of curves must agree.")
+
+    # If there are set of points to plot, get a random colour if `col_pt` is
+    # None and get the scatter plot.
     if (x is not None) and (y is not None):
         if col_pt is None:
             col_pt = [
                 "#" + "".join([random.choice("0123456789ABCDEF") for _ in range(6)])
                 for _ in range(len(x))
             ]
+        if len({len(i) for i in [x, y, col_pt]}) != 1:
+            raise ValueError("The lengths of `x`, `y` and `col_pt` must agree.")
         for i, (pt_x, pt_y) in enumerate(zip(x, y)):
             _ = ax.scatter(
                 x=pt_x,
@@ -44,11 +108,20 @@ def plot_curves(
             )
 
     for i, curve in enumerate(fittings):
+        # The curves need to be fitted
+        if not hasattr(curve, "y_fitted"):
+            raise AttributeError(f"The curve {i} has not been fitted.")
         bsp = curve.bspline_bases[0]
+        # Generate extra points at the prediction regions with the
+        # `prediction_step` parameter so the curves are plotted smoother
         x_left, x_right = thin_prediction_range(
             bspline_bases=[bsp], prediction_step=[prediction_step]
         )
+        # Define the number of points to be removed since they are in the
+        # forecasting region
         y_end = None if bsp.int_forw == 0 else -bsp.int_forw
+
+        # Plot the curves using the extended fitted coordinates
         _ = ax.plot(
             np.concatenate([x_left[0], bsp.xsample, x_right[0]]),
             np.concatenate(
@@ -63,6 +136,8 @@ def plot_curves(
             label=str(i),
         )
 
+        # If the prediction region is not empty, plot vertical dashed lines at
+        # the extremes of the fitting region
         if bsp.int_back > 0:
             _ = ax.axvline(
                 bsp.xsample[0], color=col_curve[i], linewidth=1.0, linestyle="--"
@@ -73,10 +148,9 @@ def plot_curves(
             )
         # If it is required, plot the position of the knots
         if knot_positions:
-            for i in range(len(bsp.knots[bsp.deg : -bsp.deg])):
-                _ = ax.axvline(
-                    bsp.knots[bsp.deg : -bsp.deg][i], color="grey", alpha=0.25
-                )
+            for knot in bsp.knots[bsp.deg : -bsp.deg]:
+                _ = ax.axvline(knot, color="grey", alpha=0.25)
+        # If it is required, threshold of the zero-order derivative constraints
         if constant_constraints:
             if curve.int_constraints:
                 if 0 in curve.int_constraints[0].keys():
