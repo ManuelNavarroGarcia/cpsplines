e2d7c3ecdeda7c9ae0ea59a60d75c291dc69ed75
albertotb <albertotb@gmail.com>
2021-11-02 18:17:52 +0100

    añade descripcion

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
+import pathlib
 from setuptools import find_packages, setup
+
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="cpsplines",
     license="MIT",
     version="0.1.0",
-    description="Constrained P-splines",
     packages=find_packages(),
+    description="Constrained P-splines",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="Manuel Navarro García",
     author_email="manuelnavarrogithub@gmail.com",
     url="https://github.com/ManuelNavarroGarcia/cpsplines/",
-    download_url="https://github.com/ManuelNavarroGarcia/cpsplines/archive/refs/tags/0.1.tar.gz",
+    #download_url="https://github.com/ManuelNavarroGarcia/cpsplines/archive/refs/tags/0.1.tar.gz",
     keywords=["P-splines", "MOSEK", "Python 3.9", "Constraints", "Optimization"],
     python_requires=">=3.9",
     setup_requires=["setuptools_scm"],
