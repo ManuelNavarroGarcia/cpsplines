9981d3587eef901d9457be765f16165025aa39b6
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2021-11-02 19:26:38 +0100

    recover env.yml

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author="Manuel Navarro García",
     author_email="manuelnavarrogithub@gmail.com",
     url="https://github.com/ManuelNavarroGarcia/cpsplines/",
-    # download_url="https://github.com/ManuelNavarroGarcia/cpsplines/archive/refs/tags/0.1.tar.gz",
+    download_url="https://github.com/ManuelNavarroGarcia/cpsplines/archive/refs/tags/0.1.tar.gz",
     keywords=["P-splines", "MOSEK", "Python 3.9", "Constraints", "Optimization"],
     python_requires=">=3.9",
     setup_requires=["setuptools_scm"],
