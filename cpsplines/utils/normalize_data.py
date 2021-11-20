f798bbc4724e543ec74971ed1ae079744ce2ddc1
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2021-11-20 20:23:38 +0100

    class DataNormalizer

--- cpsplines/utils/normalize_data.py
+++ cpsplines/utils/normalize_data.py
@@ -1,6 +1,7 @@
+from typing import Iterable, Union
+
 import numpy as np
 import pandas as pd
-from typing import Union
 
 
 def normalize_data(x: Union[np.ndarray, pd.Series]) -> Union[np.ndarray, pd.Series]:
@@ -20,3 +21,33 @@ def normalize_data(x: Union[np.ndarray, pd.Series]) -> Union[np.ndarray, pd.Seri
     """
 
     return (x - np.min(x)) / np.ptp(x)
+
+
+class DataNormalizer:
+    def __init__(self, feature_range: Iterable[Union[int, float]] = (0, 1)):
+        self.feature_range = feature_range
+
+    def fit(self, y: np.ndarray):
+        if self.feature_range[0] >= self.feature_range[1]:
+            raise ValueError(
+                "Minimum of desired feature range must be smaller than maximum."
+            )
+        if len(self.feature_range) != 2:
+            raise ValueError("Range must consist of two elements.")
+
+        self.y_min = np.min(y)
+        self.y_range = np.ptp(y)
+        return self
+
+    def transform(self, y: np.ndarray):
+        return (
+            self.feature_range[0]
+            + (y - self.y_min)
+            * (self.feature_range[1] - self.feature_range[0])
+            / self.y_range
+        )
+
+    def inverse_transform(self, y: np.ndarray):
+        return self.y_min + self.y_range * (y - self.feature_range[0]) / (
+            self.feature_range[1] - self.feature_range[0]
+        )
