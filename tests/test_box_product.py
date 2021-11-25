011070cd5d84b2c28165b94777ab60f1df371094
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2021-11-25 18:33:15 +0100

    test box product

---
+++ tests/test_box_product.py
@@ -0,0 +1,32 @@
+from functools import reduce
+
+import numpy as np
+import pytest
+from cpsplines.utils.box_product import box_product
+from cpsplines.utils.timer import timer
+
+B1 = np.random.random((2017, 23))
+B2 = np.random.random((2017, 17))
+B3 = np.random.random((2017, 31))
+
+
+def box_product_kron(A, B):
+    return np.multiply(
+        np.kron(A, np.ones(B.shape[1]).T), np.kron(np.ones(A.shape[1]).T, B)
+    )
+
+
+@pytest.mark.parametrize(
+    "dim",
+    [((2017, 23), (2017, 17)), ((997, 19), (997, 29), (997, 31))],
+)
+def test_box_product(dim):
+    matrices = [np.random.rand(*d) for d in dim]
+
+    with timer(tag="Using definition involving Kronecker products"):
+        exp_out = reduce(box_product_kron, matrices)
+
+    with timer(tag="Using np.tile and np.repeat"):
+        out = reduce(box_product, matrices)
+
+    np.testing.assert_allclose(exp_out, out)
