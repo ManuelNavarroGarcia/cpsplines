ffed9756332c21349806ff8c7530d10af95e27e6
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2022-01-31 18:58:11 +0100

    remove normalize_data from cpsplines

--- cpsplines/utils/normalize_data.py
+++ cpsplines/utils/normalize_data.py
@@ -1,26 +1,6 @@
 from typing import Iterable, Union
 
 import numpy as np
-import pandas as pd
-
-
-def normalize_data(x: Union[np.ndarray, pd.Series]) -> Union[np.ndarray, pd.Series]:
-
-    """
-    Normalize an array between 0 and 1.
-
-    Parameters
-    ----------
-    x : Union[np.ndarray, pd.Series]
-        The data to be normalized.
-
-    Returns
-    -------
-    Union[np.ndarray, pd.Series]
-        The data normalized.
-    """
-
-    return (x - np.min(x)) / np.ptp(x)
 
 
 class DataNormalizer:
