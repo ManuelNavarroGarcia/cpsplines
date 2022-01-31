c773dd25998022fa6f558665dec1c01692217b7c
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2022-01-31 19:01:35 +0100

    include more tests on test_normalizer

--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -4,17 +4,20 @@ from cpsplines.utils.normalize_data import DataNormalizer
 
 
 @pytest.mark.parametrize(
-    "dim",
+    "dim, derivative",
     [
-        (7,),
-        (19, 23),
-        (33, 29, 13),
+        ((7,), True),
+        ((7,), False),
+        ((19, 23), True),
+        ((19, 23), False),
+        ((33, 29, 13), True),
+        ((33, 29, 13), False),
     ],
 )
-def test_data_normalizer(dim):
+def test_data_normalizer(dim, derivative):
     y = np.random.random(dim)
     scaler = DataNormalizer()
     _ = scaler.fit(y=y)
-    y_scaled = scaler.transform(y=y)
-    y_new = scaler.inverse_transform(y=y_scaled)
+    y_scaled = scaler.transform(y=y, derivative=derivative)
+    y_new = scaler.inverse_transform(y=y_scaled, derivative=derivative)
     np.testing.assert_allclose(y, y_new)
