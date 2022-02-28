98b9baaa65c58da92d7a91ff51bdb3f6b526ffe5
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2022-02-28 16:18:46 +0100

    rename penalty matrices in test_d_matrix

--- tests/test_d_matrix.py
+++ tests/test_d_matrix.py
@@ -3,7 +3,7 @@ import pytest
 from cpsplines.psplines.bspline_basis import BsplineBasis
 from cpsplines.psplines.penalty_matrix import PenaltyMatrix
 
-D1 = np.array(
+P1 = np.array(
     [
         [1, -2, 1, 0, 0, 0, 0, 0],
         [-2, 5, -4, 1, 0, 0, 0, 0],
@@ -16,7 +16,7 @@ D1 = np.array(
     ]
 )
 
-D2 = np.array(
+P2 = np.array(
     [
         [1, -4, 6, -4, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         [-4, 17, -28, 22, -8, 1, 0, 0, 0, 0, 0, 0, 0],
@@ -34,7 +34,7 @@ D2 = np.array(
     ]
 )
 
-D3 = np.array(
+P3 = np.array(
     [
         [1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [-1, 2, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -55,16 +55,16 @@ D3 = np.array(
 # the B-spline basis construction and the arguments of the penalty (such as the
 # penalty order for discrete difference matrices)
 @pytest.mark.parametrize(
-    "x_sam, deg, n_int, prediction, ord_d, D",
+    "x_sam, deg, n_int, prediction, ord_d, P",
     [
-        (np.linspace(0, 10, 11), 3, 5, {}, 2, D1),
-        (np.linspace(-2, 12, 17), 6, 7, {}, 4, D2),
-        (np.linspace(0, 10, 11), 4, 5, {"backwards": -2.5, "forward": 11.9}, 1, D3),
+        (np.linspace(0, 10, 11), 3, 5, {}, 2, P1),
+        (np.linspace(-2, 12, 17), 6, 7, {}, 4, P2),
+        (np.linspace(0, 10, 11), 4, 5, {"backwards": -2.5, "forward": 11.9}, 1, P3),
     ],
 )
-def test_D_matrix(x_sam, deg, n_int, prediction, ord_d, D):
+def test_D_matrix(x_sam, deg, n_int, prediction, ord_d, P):
 
     bsp = BsplineBasis(deg=deg, xsample=x_sam, n_int=n_int, prediction=prediction)
     bsp.get_matrix_B()
-    D_out = PenaltyMatrix(bspline=bsp).get_penalty_matrix(**{"ord_d": ord_d})
-    np.testing.assert_allclose(D_out, D)
+    P_out = PenaltyMatrix(bspline=bsp).get_penalty_matrix(**{"ord_d": ord_d})
+    np.testing.assert_allclose(P_out, P)
