f88c1d15448b31e7114c47ae8d65ae8027db1812
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2022-07-22 10:16:08 +0200

    refactor GCV

--- cpsplines/utils/gcv.py
+++ cpsplines/utils/gcv.py
@@ -1,44 +1,9 @@
-from functools import reduce
-from typing import Iterable, Tuple, Union
+from typing import Dict, Iterable, Tuple, Union
 
 import numpy as np
-from cpsplines.utils.fast_kron import (
-    fast_kronecker_product,
-    matrix_by_tensor_product,
-    penalization_term,
-)
-
-
-def gcv_mat(
-    B_mul: Iterable[np.ndarray], D_mul: Iterable[np.ndarray]
-) -> Tuple[np.ndarray]:
-
-    """
-    Generate the matrices used to compute the hat matrix on the Generalized
-    Cross Validation.
-
-    Parameters
-    ----------
-    B_mul : Iterable[np.ndarray]
-        An iterable containing the products B_i.T @ B_i, where B_i is the design
-        matrix of the B-spline basis along the i-th axis.
-    D_mul : Iterable[np.ndarray]
-        An iterable containing the penalty matrix P_i, where P_i is the penalty
-        matrix along the i-th axis (without multiplying the smoogthing
-        parameters).
-
-    Returns
-    -------
-    Tuple[np.ndarray]
-        The matrices used in the GCV computation. The first is the B-spline
-        bases related matrix and the rest are the penalty matrices.
-    """
-    # The order must be reversed since vec(Y) = Bvec(Theta), where the
-    # vectorization is performed stacking the columns, so B is the Kronecker
-    # product of B_N kron ··· kron B_1
-    bases_term = reduce(fast_kronecker_product, B_mul[::-1])
-    penalty_term = penalization_term(matrices=D_mul[::-1])
-    return tuple([bases_term] + penalty_term)
+import statsmodels.genmod.families.family
+from cpsplines.utils.fast_kron import penalization_term, weighted_double_kronecker
+from cpsplines.utils.irls import fit_irls
 
 
 def quadratic_term(
@@ -80,26 +45,24 @@ def quadratic_term(
 
 def GCV(
     sp: Iterable[Union[int, float]],
-    B_weighted: Iterable[np.ndarray],
-    Q_matrices: Iterable[np.ndarray],
-    y: np.ndarray,
+    obj_matrices: Dict[str, Union[np.ndarray, Iterable[np.ndarray]]],
     family: str = "gaussian",
 ) -> float:
 
-    """Computes the Generalized Cross Validation (Golub et al., 1979).
+    """
+    Computes the Generalized Cross Validation (Golub et al., 1979).
 
     Parameters
     ----------
     sp : Iterable[Union[int, float]]
         The smoothing paramater vector.
-    B_weighted : Iterable[np.ndarray]
-        The weighted design matrices from B-spline basis.
-    Q_matrices : Iterable[np.ndarray]
-        The array of matrices of the matrices to be inverted. The first is
-        related to the B-spline bases and the rest to the penalty terms along
-        each dimension.
-    y : np.ndarray
-        The response variable sample.
+    obj_matrices : Dict[str, Union[np.ndarray, Iterable[np.ndarray]]]
+        A dictionary containing the necessary arrays (the basis matrices, the
+        penalty matrices and the response variable sample) used to compute the
+        quadratic terms in the objective function.
+    family : statsmodels.genmod.families.family, optional
+        The specific exponential family distribution where the response variable
+        belongs to, by default "gaussian".
 
     References
     ----------
@@ -112,6 +75,7 @@ def GCV(
     float
         The GCV value.
     """
+
     if family == "gaussian":
         family = statsmodels.genmod.families.family.Gaussian()
     elif family == "poisson":
@@ -119,6 +83,13 @@ def GCV(
     else:
         raise ValueError(f"Family {family} is not implemented.")
 
+    bases_term, penalty_term = quadratic_term(
+        sp=sp, obj_matrices=obj_matrices, family=family
+    )
+    y_hat = fit_irls(
+        obj_matrices=obj_matrices, family=family, penalty_term=penalty_term
+    )
+    # Return the GCV value, which is n * RSS / (n - tr(H))**2, where RSS is the
     # residual sum of squares, n is the product of the dimensions of y and H is
     # the hat matrix of the unconstrained problem
     return (
