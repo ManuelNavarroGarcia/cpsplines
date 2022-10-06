675b357ee948f634f89d2b831ad59b833725bea0
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2022-10-06 16:02:48 +0200

    define scatter to grid

--- cpsplines/utils/rearrange_data.py
+++ cpsplines/utils/rearrange_data.py
@@ -57,3 +57,55 @@ def grid_to_scatter(
     )
 
 
+def scatter_to_grid(
+    data: pd.DataFrame, y_col: str
+) -> Tuple[List[np.ndarray], np.ndarray]:
+
+    """
+    Given a DataFrame containing the value of the covariates and the target
+    variable, generates a list of arrays containing the value of the covariates
+    and a grid of target values with these coordinates.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Input data.
+    y_col : str
+        Column name of the target variable.
+
+    Returns
+    -------
+    Tuple[List[np.ndarray], np.ndarray]
+        A tuple containing the coordinates as the first element and the array
+        of response values a the second element.
+
+    Raises
+    ------
+    ValueError
+        If multiple responses are found for the same coordinates.
+
+    References
+    ----------
+    - https://stackoverflow.com/a/35049899
+    """
+
+    # Get regressor columns
+    x_cols = data.columns.drop(y_col).tolist()
+    if len(x_cols) > 1:
+        # The indexes are now the coordinates, and the values the response sample
+        df = data.groupby(x_cols).mean()
+        if len(data) != len(df):
+            raise ValueError(
+                "Multiple responses for the same coordinates. Data cannot be rearranged into a grid."
+            )
+        # Create an empty array of NaN of the right dimensions
+        arr = np.full(tuple(map(len, df.index.levels)), np.nan)
+        # Fill it using Numpy's advanced indexing
+        arr[tuple(df.index.codes)] = df.values.flat
+        # Get the first unique values for each axis
+        x = [np.unique(row) for row in np.array(df.index.tolist()).T]
+        y = arr.copy()
+    else:
+        x = [data[x_cols].values.flatten()]
+        y = data[y_col].values
+    return x, y
