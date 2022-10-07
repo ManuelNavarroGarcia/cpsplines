484d704644f8ab4ac3875cdb8ea15b3618ce6b3e
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2022-10-07 11:38:51 +0200

    import matrix_by_tensor_product

--- cpsplines/mosek_functions/point_constraints.py
+++ cpsplines/mosek_functions/point_constraints.py
@@ -3,6 +3,7 @@ from typing import Dict, Iterable, Tuple
 import mosek.fusion
 import numpy as np
 import pandas as pd
+from cpsplines.mosek_functions.utils_mosek import matrix_by_tensor_product_mosek
 from cpsplines.psplines.bspline_basis import BsplineBasis
 from cpsplines.utils.rearrange_data import scatter_to_grid
 
