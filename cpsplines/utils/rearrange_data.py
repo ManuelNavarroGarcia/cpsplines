08d8ed01f2effcd1c51992e2cc318d8af5f86d5e
ManuelNavarroGarcia <manuelnavarrogithub@gmail.com>
2022-11-16 21:56:32 +0100

    replace ValueError by RearrangingError

--- cpsplines/utils/rearrange_data.py
+++ cpsplines/utils/rearrange_data.py
@@ -103,7 +103,7 @@ def scatter_to_grid(
         # The indexes are now the coordinates, and the values the response sample
         df = data.groupby(x_cols).mean()
         if len(data) != len(df):
-            raise ValueError(
+            raise RearrangingError(
                 "Multiple responses for the same coordinates. Data cannot be rearranged into a grid."
             )
         # Create an empty array of NaN of the right dimensions
